dfe815835da33332911c5281bece5bcc422f2370
Striker528 <austyn.j.adams@gmail.com>
2022-10-10 18:01:40 -0700

    Finsihed Basic Dashboard UI for Movie & Actor

--- udemy_movie_review_app/frontend/src/components/LatestUploads.jsx
+++ udemy_movie_review_app/frontend/src/components/LatestUploads.jsx
@@ -3,59 +3,83 @@ import React from 'react';
 import {BsTrash, BsPencilSquare, BsBoxArrowUpRight} from "react-icons/bs";
 
 export default function LatestUploads() {
+    //need to wrap the avatar in 2 curly braces
   return (
       <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
           <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
               Recent Uploads
           </h1>
 
-          <MovieListItem/>
+          <MovieListItem
+              movie={{
+                  poster: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Mops_oct09_cropped2.jpg",
+                  title: "Holder title",
+                  status: "public",
+                  genres: ["Action", "Comedy"]
+              }}
+          />
+
       </div>
   )
 }
 
-const MovieListItem = () => {
+const MovieListItem = ({ movie, onDeleteClick, onEditClick, onOpenCLick }) => {
+    //take in a profile, then destructure the avatar
+    //genres will be an array, make it default to [] to avoid errors
+    const { poster, title, genres = [], status } = movie;
+    
+    //for the genres map
+    //if we set the key to just be {g} == get issues
+    //need to add the index, and so must also destructure the index
     return (
         <table className="w-full border-b">
             <tbody>
                 <tr>
                     <td>
                         <div className="w-24">
-                            <img className="w-full aspect-video" src ="https://upload.wikimedia.org/wikipedia/commons/f/f0/Mops_oct09_cropped2.jpg" alt =""/>
+                            <img
+                                className="w-full aspect-video"
+                                src={poster}
+                                alt={title}
+                            />
                         </div>
                     </td>
 
                     <td className="w-full pl-5">
                         <div>
                             <h1 className=" text-lg font-semibold text-primary dark:text-white">
-                                Title Ex 1
+                                {title}
                             </h1>
                             <div className="space-x-1">
-                                <span className="text-primary dark:text-white text-xs">
-                                    Action
-                                </span>
-                                <span className="text-primary dark:text-white text-xs">
-                                    Drama
-                                </span>
+                                {genres.map((g,index) => {
+                                    return (
+                                        <span
+                                            key={g + index}
+                                            className="text-primary dark:text-white text-xs">
+                                            {g}
+                                        </span>
+                                    )
+                                })}
+                                
                             </div>
                         </div>
                     </td>
 
                     <td className="px-5">
                         <p className="text-primary dark:text-white">
-                            public
+                            {status}
                         </p>
                     </td>
 
                     <td>
                         <div className="flex items-center space-x-3 text-primary dark:text-white text-lg">
-                            <button type="button">
+                            <button onClick={onDeleteClick} type="button">
                                 <BsTrash/>
                             </button>
-                            <button type="button">
+                            <button onClick={onEditClick} type="button">
                                 <BsPencilSquare/>
                             </button>
-                            <button type="button">
+                            <button onClick={onOpenCLick} type="button">
                                 <BsBoxArrowUpRight/>
                             </button>
                         </div>
--- udemy_movie_review_app/frontend/src/components/admin/Actors.jsx
+++ udemy_movie_review_app/frontend/src/components/admin/Actors.jsx
@@ -1,5 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {BsTrash, BsPencilSquare, BsBoxArrowUpRight} from "react-icons/bs";
 
 export default function Actors() {
-  return <div>Actors</div>;
+  /*
+  profile={{
+          avatar:"https://lumiere-a.akamaihd.net/v1/images/boba-fett-main_a8fade4d.jpeg?region=205%2C34%2C1064%2C598&width=960",
+          name:"Boba Fett",
+          about:"The absolute best",
+        }}
+  */
+
+  return (
+    <div className="grid grid-cols-4 gap-3 my-5">
+      <ActorProfile
+        profile={{
+          avatar:"https://lumiere-a.akamaihd.net/v1/images/boba-fett-main_a8fade4d.jpeg?region=205%2C34%2C1064%2C598&width=960",
+          name:"Boba Fett",
+          about:"The absolute best",
+        }}
+      />
+    </div>
+  )
+}
+
+//when taking something(s) in, alwasy use {} around it
+const ActorProfile = ({profile}) => {
+  //create a new state for showOptions, want to show and hide options
+  //default value is false
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleoOnMouseEnter = () => {
+    setShowOptions(true);
+  }
+
+  const handleoOnMouseLeave = () => {
+    setShowOptions(false);
+  }
+
+  //if no profile, don't want to render anything
+  if (!profile) return null;
+
+  const {name, avatar, about = ""} = profile
+
+  return(
+    <div className="bg-white shadow dark:shadow dark:bg-secondary rounded h-20 overflow-hidden">
+      <div
+        onMouseEnter={handleoOnMouseEnter}
+        onMouseLeave={handleoOnMouseLeave}
+        className="flex cursor-pointer relative"
+      >
+        <img
+          src={avatar}
+          alt=""
+          className="w-20 aspect-square object-cover"
+        />
+        
+        <div className="px-2">
+          <h1
+            className="text-xl text-primary dark:text-white font-semibold">
+            {name}
+          </h1>
+          <p
+            className="dark:text-white font-semibold">
+            {about.substring(0,50)}
+          </p>
+        </div>
+
+        <Options visible={showOptions} />
+      </div>
+    </div>
+  )
+}
+
+const Options = ({ visible, onDeleteClick, onEditClick }) => {
+  if (!visible) return null; 
+  
+  return (
+    <div
+      className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm flex justify-center items-center space-x-5"
+    >
+      <button
+        onClick={onDeleteClick}
+        className="p-2 rounded-full bg-red-600 text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsTrash/>
+      </button>
+      <button
+        onClick={onEditClick}
+        className="p-2 rounded-full bg-green-600  text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsPencilSquare />
+      </button>
+    </div>
+  )
 }
